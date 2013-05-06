c3258d0048217bacf3b55fb60b185b760fc3172a
Ryan <lightmywifire@gmail.com>
2013-05-06 00:22:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '976e6924bf49bafb5ad33e347d334a089bc6a3160fc5fd80154b98c944a54ff992f5ba834c424ca6b145c20ac186a55312371847cd3c63f37692fd7e7fb50ee4'
